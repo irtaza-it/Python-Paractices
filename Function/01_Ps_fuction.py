324d25c7b05e39e09644749d42f0c2264252f45b
Irtaza <irtaza.it.shabbir@gmail.com>
2025-12-23 22:36:02 +0500

    function

--- Function/01_Ps_fuction.py
+++ Function/01_Ps_fuction.py
@@ -1,80 +1,91 @@
-# def calc_sum(a , b):
-#     sum = a + b
-#     print(sum)
-#     return sum
+'''Qno 1'''
 
-# calc_sum(5 , 3) 
+def calc_sum(a , b):
+    sum = a + b
+    print(sum)
+    return sum
 
-# def average():
-#     a = int(input("Enter number: "))
-#     b = int(input("Enter number: "))
-#     c = int(input("Enter number: "))
-#     average = (a + b + c) /3
-#     print(average)
-
-# average()
-
-# cities = ["Lahore", "Karachi", "Islamabad", "Faisalabad", "RawalPindi"]
-# heroes = ["Marvel", "Spiderman", "Joker", "Superman"]
-# color = ["Black", "White", "Blue", "Purple", "Orange", "Red", "Green"]
-
-# def print_len(list):
-#     print(len(list))
-
-# def print_list(list):
-#     for item in list:
-#         print(item)
-
-# print_list(color)
-
-# print_len(cities)
-
-# n = int(input("Enter number: "))
-
-# def calc_fact(n):
-#     fact = 1
-#     for i in range(1, n+1):
-#         fact *= i
-#     print(fact)
-
-# calc_fact(n)
+calc_sum(5 , 3) 
 
 
-# usd = int(input("Enter USD: "))
+'''Qno 2'''
 
-# def converter(usd):
-#     pkr = usd * 280
-#     print(pkr)
+def average():
+    a = int(input("Enter number: "))
+    b = int(input("Enter number: "))
+    c = int(input("Enter number: "))
+    average = (a + b + c) /3
+    print(average)
 
-# converter(usd)
-
-
-
-# num = int(input("Enter number: "))
-
-# def find(num):
-#     if num % 2 == 0:
-#         return("Even")
-#     else:
-#         return("Odd")
-
-# result = find(num)
-# print(result)
+average()
 
 
-# def find(num):
-#     print("Even" if num%2 == 0 else "Odd")
+'''Qno 3'''
 
-# num = int(input("Enter number: "))
+cities = ["Lahore", "Karachi", "Islamabad", "Faisalabad", "RawalPindi"]
+heroes = ["Marvel", "Spiderman", "Joker", "Superman"]
+color = ["Black", "White", "Blue", "Purple", "Orange", "Red", "Green"]
 
-# find(num)
+def print_len(list):
+    print(len(list))
+
+def print_list(list):
+    for item in list:
+        print(item)
+
+print_list(color)
+
+print_len(cities)
 
 
-# def calc_sum(a,b):
-#     sum = (a+b)
-#     print(sum)
+'''Qno 4'''
 
-# calc_sum(5,6)
+n = int(input("Enter number: "))
+
+def calc_fact(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(fact)
+
+calc_fact(n)
 
 
+'''Qno 5'''
 
+usd = int(input("Enter USD: "))
+
+def converter(usd):
+    pkr = usd * 280
+    print(pkr)
+
+converter(usd)
+
+'''Qno 6'''
+
+num = int(input("Enter number: "))
+
+def find(num):
+    if num % 2 == 0:
+        return("Even")
+    else:
+        return("Odd")
+
+result = find(num)
+print(result)
+
+'''Qno 7'''
+
+def find(num):
+    print("Even" if num%2 == 0 else "Odd")
+
+num = int(input("Enter number: "))
+
+find(num)
+
+
+def calc_sum(a,b):
+    sum = (a+b)
+    print(sum)
+
+calc_sum(5,6)
--- Function/PB_Function.py
+++ Function/PB_Function.py
@@ -1,23 +1,23 @@
 '''Qno 1'''
 
-# def cal_sum():
-#     a = int(input("Enter a number: "))
-#     b = int(input("Enter a number: "))
-#     sum = (a+b)
-#     print(f"Sum of {a} and {b} is: {a+b}")
+def cal_sum():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    sum = (a+b)
+    print(f"Sum of {a} and {b} is: {a+b}")
 
 # cal_sum()
 
 '''Qno 2'''
 
-# def is_even():
-#     a = int(input("Enter a number: "))
-#     if a % 2 == 0:
-#         print(True)
-#     else:
-#         print(False)
+def is_even():
+    a = int(input("Enter a number: "))
+    if a % 2 == 0:
+        print(True)
+    else:
+        print(False)
 
-# is_even()
+is_even()
 
 '''Qno 3'''
 
@@ -39,5 +39,56 @@ def factorial(n):
         result *= i
     return result
 
-print(factorial(5))
+print(factorial(6))
 
+
+'''Qno 5'''
+
+def gerater_number():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    if num1 > num2:
+        return f"Greater number is {num1}"
+    elif num2 > num1:
+        return f"Greater number is {num2}"
+    else:
+        return "Both number are equal"
+
+print(gerater_number())
+
+
+'''Qno 6'''
+
+def count_vowels():
+    vowels = "aeiou"
+    count = 0
+    word = input("Enter a word: ").lower()
+    for chr in word:
+        if chr in vowels:
+            count += 1
+    return count
+print(count_vowels())
+
+
+'''Qno 7'''
+
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+user_input = input("Enter a number with spacing: ")
+
+numbers = list(map(int, user_input.split()))
+
+print(numbers)
+
+
+'''Qno 8'''
+
+def reverse_string():
+    text = input("Enter a text: ")
+    return text[:: -1]
+
+print(reverse_string())
